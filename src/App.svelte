e78789333318c97faf22adc52b325f57fcb699b9
Mihail Pertsev <arthasmontano@mail.ru>
2020-10-04 16:57:50 +0300

    works

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,17 @@
 <script>
-  let count = 0;
-  function handleClick() {
-    count++;
-  }
+  import { FirebaseApp, User } from "sveltefire";
+  import firebase from "firebase/app";
+  import "firebase/auth";
+  import "firebase/storage"; //this line is causing an error "instance.INTERNAL is undefined"
 
-  let disabled = true;
+  const firebaseConfig = {};
+  firebase.initializeApp(firebaseConfig);
 </script>
 
-<style>
-  .disabled {
-    background: gray;
-  }
-
-  .img {
-    width: 240px;
-  }
-</style>
-
-<main class="bg-red-700">
-  <button class="btn" class:disabled on:click={handleClick}> Click </button>
-  <p>{count}</p>
-</main>
-
-<img
-  class="img"
-  src="https://images.unsplash.com/photo-1593642634315-48f5414c3ad9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-  alt="" />
+<FirebaseApp {firebase}>
+  <main class="bg-red-700 text-center">
+    <User let:auth let:user>
+      <div slot="signed-out">Signed out</div>
+    </User>
+  </main>
+</FirebaseApp>
